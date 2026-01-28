9ee3f47fcadd1c806c932d2d9fee6e1a5d24ccd7
vaniapzm <vaniaperezmelgar20@gmail.com>
2026-01-28 12:59:11 -0500

    Add report review functionality

--- movies/views.py
+++ movies/views.py
@@ -58,6 +58,6 @@ def edit_review(request, id, review_id):
 
 @login_required
 def delete_review(request, id, review_id):
-    review = get_object_or_404(Review, id=review_id, user=request.user)
+    review = get_object_or_404(Review, id=review_id)
     review.delete()
     return redirect('movies.show', id=id)
