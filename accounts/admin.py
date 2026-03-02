9aac8de053690687af0ac1fcbfff2c182b31401f
peuch <peu.chen7@gmail.com>
2026-03-02 01:26:51 -0500

    Added US3

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import UserMostPurchase
+from django.db.models import Value, Sum
+from django.db.models.functions import Coalesce
 
 # Register your models here.
+class UserMostPurchaseAdmin(admin.ModelAdmin):
+    readonly_fields = ["getTotalPurchases"]
+    exclude = ('password', "last_login", "groups", "is_superuser", "is_staff", "user_permissions", "date_joined")
+    show_in_index = True
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(
+            request
+        ).filter(
+            is_staff = False, 
+            is_active = True
+        ).annotate(
+            item_count = Coalesce(
+                Sum('order__item__quantity'),
+                Value(0),
+            )
+        ).order_by(
+            '-item_count'
+        )
+
+        max_subquery = qs.values("item_count")[:1]
+        print(max_subquery)
+        return qs.filter(item_count = max_subquery)
+    
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+admin.site.register(UserMostPurchase, UserMostPurchaseAdmin)
--- cart/models.py
+++ cart/models.py
@@ -7,6 +7,10 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User,
         on_delete=models.CASCADE)
+    
+    def getItems(self):
+        return Item.objects.filter(order=self)
+    
     def __str__(self):
         return str(self.id) + ' - ' + self.user.username
 
